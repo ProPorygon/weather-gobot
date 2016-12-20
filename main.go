fb617d23d2de5324bed705b59671d661ca397ea0
Kevin Renner <krenner529@gmail.com>
2016-12-20 03:27:53 -0500

    fixed retrieving body data
    removed extra print

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	var responseData map[string]interface{}
-	json.NewDecoder(r.Body).Decode(responseData)
+	json.NewDecoder(r.Body).Decode(&responseData)
 	if responseData["text"] == "weatherbot" {
 		jsonMap := map[string]string{"bot_id": os.Getenv("BOT_ID"), "text": "Hello"}
 		jsonVal, _ := json.Marshal(jsonMap)
