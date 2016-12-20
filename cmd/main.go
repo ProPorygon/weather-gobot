9f8d9b6aeec6238236a7d34c215661e85caa5af9
Kevin Renner <krenner529@gmail.com>
2016-12-20 01:45:10 -0500

    bot id in env variable

---
+++ cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	var responseData map[string]interface{}
+	json.NewDecoder(r.Body).Decode(responseData)
+	jsonMap := map[string]string{"bot_id": os.Getenv("BOT_ID"), "text": "Hello"}
+	jsonVal, _ := json.Marshal(jsonMap)
+	_, err := http.Post("https://api.groupme.com/v3/bots/post",
+		"application/json", bytes.NewReader(jsonVal))
+	if err != nil {
+		log.Printf("%v", err.Error())
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
