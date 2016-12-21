644a7c627b3c12886f92dc451b15863e2baaa836
Kevin Renner <krenner529@gmail.com>
2016-12-21 02:08:12 -0500

    added weather api

--- main.go
+++ main.go
@@ -6,19 +6,59 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
+
+type Weather struct {
+	Currently CurWeather
+}
+
+type CurWeather struct {
+	Summary     string
+	Temperature float64
+}
+
+func getWeatherString() string {
+	url := "https://api.darksky.net/forecast/" + os.Getenv("DARKSKY_KEY") + "/40.1164,-88.2434"
+	log.Println(url)
+	res, err := http.Get(url)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	if res != nil {
+		defer res.Body.Close()
+	}
+	weather := &Weather{
+		Currently: CurWeather{
+			Summary:     "summary",
+			Temperature: 0.0,
+		},
+	}
+	if err := json.NewDecoder(res.Body).Decode(&weather); err != nil {
+		log.Println(err.Error())
+	}
+	summary := weather.Currently.Summary
+	temperature := weather.Currently.Temperature
+	response := "The current weather is " + summary + ", with a temperature of " + strconv.FormatFloat(temperature, 'f', 2, 32)
+	return response
+}
+
+func postToChat(message string) {
+	jsonMap := map[string]string{"bot_id": os.Getenv("BOT_ID"), "text": message}
+	jsonVal, _ := json.Marshal(jsonMap)
+	_, err := http.Post("https://api.groupme.com/v3/bots/post",
+		"application/json", bytes.NewReader(jsonVal))
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	var responseData map[string]interface{}
 	json.NewDecoder(r.Body).Decode(&responseData)
 	if responseData["text"] == "weatherbot" {
-		jsonMap := map[string]string{"bot_id": os.Getenv("BOT_ID"), "text": "Hello"}
-		jsonVal, _ := json.Marshal(jsonMap)
-		_, err := http.Post("https://api.groupme.com/v3/bots/post",
-			"application/json", bytes.NewReader(jsonVal))
-		if err != nil {
-			log.Printf("%v", err.Error())
-		}
+		message := getWeatherString()
+		postToChat(message)
 	}
 }
 
